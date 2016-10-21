e2830a941ae0b53bb2488a9ce2de790549362281
Peter van Zetten <peter.van.zetten@cgi.com>
2016-10-21 11:25:00 +0100

    Add Dockerfile for build environment
    Change-Id: I3b868151b52c6611c23d74843225c6fa362d895b

--- build/pipeline.groovy
+++ build/pipeline.groovy
@@ -2,6 +2,12 @@
     node('docker') {
         deleteDir()
         unstash 'source'
+
+        def buildImg
+        stage('Bake build container') {
+            buildImg = docker.build('eossci/f-tep_build', '--build-arg http_proxy --build-arg https_proxy build/')
+        }
+
     }
 }
 

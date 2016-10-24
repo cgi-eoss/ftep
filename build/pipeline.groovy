6b7933c29adbe96cecadfe19c12dded8302b5890
Peter van Zetten <peter.van.zetten@cgi.com>
2016-10-24 12:34:33 +0100

    Import ZOO-Project for distribution
    Add the ZOO-Project 1.5.0 sources. Add dependencies to the build
    container and add build steps to the pipeline.
    
    Change-Id: I9879d0fa42b65db50bacf1b43fe9713b73bfa2bb

--- build/pipeline.groovy
+++ build/pipeline.groovy
@@ -3,11 +3,19 @@
         deleteDir()
         unstash 'source'
 
+        // prep the distribution assembly directory
+        env.DISTDIR = "${WORKSPACE}/.dist"
+        sh "mkdir -p ${env.DISTDIR}"
+
         def buildImg
         stage('Bake build container') {
             buildImg = docker.build('eossci/f-tep_build', '--build-arg http_proxy --build-arg https_proxy build/')
         }
 
+        buildImg.inside {
+            def zp = load 'build/zoo-project.pipeline.groovy'
+            zp.build()
+        }
     }
 }
 

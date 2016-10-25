1140c7c1c32cebf68703c2dce2ffdae742de154a
Peter van Zetten <peter.van.zetten@cgi.com>
2016-10-25 11:31:33 +0100

    Initial import of F-TEP Maven project
    Copy existing project as-is, to be reorganised in the future.
    
    Change-Id: I0311329e045ceca8d46e90c5099f54dc196511dd

---
+++ ftep-subsys/ftep-subsys-core/ftep-subsys-core-utils/src/main/java/com/cgi/eoss/ftep/core/utils/rest/resources/ResourceLogin.java
@@ -0,0 +1,68 @@
+package com.cgi.eoss.ftep.core.utils.rest.resources;
+
+import com.github.jasminb.jsonapi.annotations.Id;
+import com.github.jasminb.jsonapi.annotations.Type;
+
+@Type("login")
+public class ResourceLogin {
+
+  @Id
+  private String id;
+
+  // Attributes in HTTP request
+  private String user;
+  private String password;
+
+  // Attributes in response
+  private String sessionId;
+  private String sessionName;
+  private String token;
+
+
+  public String getUser() {
+    return user;
+  }
+
+  public void setUser(String user) {
+    this.user = user;
+  }
+
+  public String getToken() {
+    return token;
+  }
+
+  public void setToken(String token) {
+    this.token = token;
+  }
+
+  public String getSessionName() {
+    return sessionName;
+  }
+
+  public void setSessionName(String sessionName) {
+    this.sessionName = sessionName;
+  }
+
+  public String getSessionId() {
+    return sessionId;
+  }
+
+  public void setSessionId(String sessionId) {
+    this.sessionId = sessionId;
+  }
+
+  @Override
+  public String toString() {
+    return "Login{" + "id='" + id + '\'' + ", user='" + user + '\'' + ", sessionId=" + sessionId
+        + ", sessionName=" + sessionName + ", token=" + token + '}';
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+}

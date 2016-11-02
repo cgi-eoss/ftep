9b566377b0ab968f550e08ad9ba0ce14be0a6ae5
Peter van Zetten <peter.van.zetten@cgi.com>
2016-11-02 16:31:13 +0000

    Add standalone puppet conf to distribution
    Download and install puppet modules to the staging dir, and package into
    the dist directory.
    
    Change-Id: I29788a35f155e4b082f8b59cf0a0f861351f187d

--- build/pipeline.groovy
+++ build/pipeline.groovy
@@ -14,27 +14,26 @@
             buildImg = docker.build('eossci/f-tep_build', '--build-arg http_proxy --build-arg https_proxy build/')
         }
 
-        // Expose the docker socket to the container, so it may itself create and use sibling containers
-        def dockerArgs = '-v /var/run/docker.sock:/var/run/docker.sock -e http_proxy -e https_proxy'
+        def dockerArgs = "-e http_proxy -e https_proxy -e HOME=${WORKSPACE}/.home"
         buildImg.inside(dockerArgs) {
-            // Build F-TEP
-            stage('Backend') {
-                configFileProvider([configFile(fileId: '2c353f8f-d8c7-41e8-b26f-8f76dfa4a000', variable: 'M2SETTINGS')]) {
-                    def m2args = "-B -s ${M2SETTINGS} -Dmaven.repo.local=${WORKSPACE}/.repository"
-                    sh """
-                        mvn ${m2args} clean install
-                        cp ftep-config/target/rpm/f-tep-processors/RPMS/noarch/*.rpm ${env.DISTDIR_NOARCH}/
-                    """
+            wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
+                // Build F-TEP
+                stage('Backend') {
+                    configFileProvider([configFile(fileId: '2c353f8f-d8c7-41e8-b26f-8f76dfa4a000', variable: 'M2SETTINGS')]) {
+                        def m2args = "-B -s ${M2SETTINGS} -Dmaven.repo.local=${WORKSPACE}/.repository"
+                        sh """
+                            mvn ${m2args} clean install
+                            cp ftep-config/target/rpm/f-tep-processors/RPMS/noarch/*.rpm ${env.DISTDIR_NOARCH}/
+                        """
+                    }
                 }
+
+                // Build third-party components
+                load('build/zoo-project.pipeline.groovy').build()
+
+                // Build full standalone distribution (and archive the result)
+                load('build/standalone-dist.pipeline.groovy').build()
             }
-
-            // Build third-party components
-            load('build/zoo-project.pipeline.groovy').build()
-
-            // Create yum repository
-            sh "createrepo ${env.DISTDIR}/repo"
         }
-
-        archiveArtifacts artifacts: '.dist/**/*', fingerprint: true, allowEmptyArchive: true
     }
 }

937dfc47414c28ed15cbb255dd9532dbc7e1e956
Peter van Zetten <peter.van.zetten@cgi.com>
2016-11-02 16:31:13 +0000

    Add standalone puppet conf to distribution
    Download and install puppet modules to the staging dir, and package into
    the dist directory.
    
    Change-Id: I29788a35f155e4b082f8b59cf0a0f861351f187d

---
+++ build/standalone-dist.pipeline.groovy
@@ -0,0 +1,24 @@
+def build() {
+
+    def puppetPath = "${WORKSPACE}/distribution/puppet"
+    def modulePath = "${puppetPath}/modules"
+
+    stage('Standalone Distribution') {
+        sh """
+            createrepo ${env.DISTDIR}/repo
+        """
+
+        dir("${modulePath}/ftep") {
+            git url: 'https://github.com/cgi-eoss/puppet-ftep', branch: 'master', changelog: false, poll: false
+        }
+        sh """
+            puppet module install --modulepath=${modulePath} puppetlabs-stdlib
+            cd ${puppetPath} && tar cfz ${env.DISTDIR}/puppet.tar.gz .
+        """
+    }
+
+    archiveArtifacts artifacts: '.dist/**/*', fingerprint: true, allowEmptyArchive: true
+}
+
+return this;
+

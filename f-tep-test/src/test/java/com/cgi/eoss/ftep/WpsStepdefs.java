67803a3e693b1804d00ffa2ec5ac2c4f548f567c
Peter van Zetten <peter.van.zetten@cgi.com>
2016-12-08 16:50:14 +0000

    Acceptance test harness
    Create a docker-compose acceptance test harness and implement the
    existing Cucumber spec stub using the real container.
    
    Change-Id: I2471ce6e3b586429f425aa4f1004d474a09a9dae

--- f-tep-test/src/test/java/com/cgi/eoss/ftep/WpsStepdefs.java
+++ f-tep-test/src/test/java/com/cgi/eoss/ftep/WpsStepdefs.java
@@ -1,25 +1,68 @@
 package com.cgi.eoss.ftep;
 
-import cucumber.api.PendingException;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.javanet.NetHttpTransport;
 import cucumber.api.java8.En;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.shaded.com.google.common.io.ByteStreams;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
+
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 /**
  */
 public class WpsStepdefs implements En {
+    private DockerComposeContainer environment;
+
+    private final HttpRequestFactory httpRequestFactory = new NetHttpTransport().createRequestFactory();
+
+    private String responseBody;
+
     public WpsStepdefs() {
         Given("^the F-TEP backend$", () -> {
-            // Write code here that turns the phrase above into concrete actions
-            throw new PendingException();
+            environment = new DockerComposeContainer(Paths.get(System.getProperty("HARNESS_DIR"), "docker-compose.yml").toFile())
+                    .withExposedService("backend_1", 80);
+            environment.starting(null);
         });
 
         When("^a user requests GetCapabilities from WPS$", () -> {
-            // Write code here that turns the phrase above into concrete actions
-            throw new PendingException();
+            HttpResponse response = getBackendHttpResponse("/cgi-bin/zoo_loader.cgi?request=GetCapabilities&service=WPS");
+            assertThat(response.getStatusCode(), is(200));
+            responseBody = getResponseContent(response);
         });
 
         Then("^they receive the F-TEP service list$", () -> {
-            // Write code here that turns the phrase above into concrete actions
-            throw new PendingException();
+            assertThat(responseBody, allOf(
+                    containsString("<ows:Title>Forestry TEP (F-TEP)  WPS Server</ows:Title>"),
+                    containsString("<ows:Identifier>QGIS</ows:Identifier>")
+            ));
         });
+    }
+
+    private String getResponseContent(HttpResponse response) {
+        try(InputStream is = response.getContent()) {
+            return new String(ByteStreams.toByteArray(is));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private HttpResponse getBackendHttpResponse(String path) {
+        try {
+            return httpRequestFactory.buildGetRequest(new GenericUrl("http://" +
+                    environment.getServiceHost("backend_1", 80) + ":" +
+                    environment.getServicePort("backend_1", 80) +
+                    path)).execute();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

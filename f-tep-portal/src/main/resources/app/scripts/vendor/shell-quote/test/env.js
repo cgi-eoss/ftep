a1e453bf8cbad798bc6a1ed7d90ab0345ac70712
Peter van Zetten <peter.van.zetten@cgi.com>
2016-12-31 11:40:24 +0000

    Commit all JS dependencies
    Add all javascript dependencies to the repo to remove the newtork
    dependency during the build. The JS dependencies can be updated by
    modifying the portal build.gradle and running the "clientRefresh" task.
    
    Change-Id: Ifb1ea4f628d726253bca82e8f936d5c879e41250

---
+++ f-tep-portal/src/main/resources/app/scripts/vendor/shell-quote/test/env.js
@@ -0,0 +1,28 @@
+var test = require('tape');
+var parse = require('../').parse;
+
+test('expand environment variables', function (t) {
+    t.same(parse('a $XYZ c', { XYZ: 'b' }), [ 'a', 'b', 'c' ]);
+    t.same(parse('a${XYZ}c', { XYZ: 'b' }), [ 'abc' ]);
+    t.same(parse('a${XYZ}c $XYZ', { XYZ: 'b' }), [ 'abc', 'b' ]);
+    t.same(parse('"-$X-$Y-"', { X: 'a', Y: 'b' }), [ '-a-b-' ]);
+    t.same(parse("'-$X-$Y-'", { X: 'a', Y: 'b' }), [ '-$X-$Y-' ]);
+    t.same(parse('qrs"$zzz"wxy', { zzz: 'tuv' }), [ 'qrstuvwxy' ]);
+    t.same(parse("qrs'$zzz'wxy", { zzz: 'tuv' }), [ 'qrs$zzzwxy' ]);
+    t.same(parse("qrs${zzz}wxy"), [ 'qrswxy' ]);
+    t.same(parse("ab$x", { x: 'c' }), [ 'abc' ]);
+    t.same(parse("ab\\$x", { x: 'c' }), [ 'ab$x' ]);
+    t.same(parse("ab${x}def", { x: 'c' }), [ 'abcdef' ]);
+    t.same(parse("ab\\${x}def", { x: 'c' }), [ 'ab${x}def' ]);
+    t.same(parse('"ab\\${x}def"', { x: 'c' }), [ 'ab${x}def' ]);
+    
+    t.end();
+});
+
+test('environment variables with metacharacters', function (t) {
+    t.same(parse('a $XYZ c', { XYZ: '"b"' }), [ 'a', '"b"', 'c' ]);
+    t.same(parse('a $XYZ c', { XYZ: '$X', X: 5 }), [ 'a', '$X', 'c' ]);
+    t.same(parse('a"$XYZ"c', { XYZ: "'xyz'" }), [ "a'xyz'c" ]);
+    
+    t.end();
+});

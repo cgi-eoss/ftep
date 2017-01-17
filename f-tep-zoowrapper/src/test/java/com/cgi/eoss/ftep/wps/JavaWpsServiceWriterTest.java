cb5b57fd28ce93d77b2a9d3c5f8d7d39e7a3e694
Peter van Zetten <peter.van.zetten@cgi.com>
2017-01-17 11:59:39 +0000

    Add non-throwing test for generated WPS impl class
    Change-Id: Ia1ff4866c38be44cc2abb71b05fba7aad0faa54e

--- f-tep-zoowrapper/src/test/java/com/cgi/eoss/ftep/wps/JavaWpsServiceWriterTest.java
+++ f-tep-zoowrapper/src/test/java/com/cgi/eoss/ftep/wps/JavaWpsServiceWriterTest.java
@@ -13,13 +13,18 @@ import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.FileSystem;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.isA;
 import static org.hamcrest.Matchers.hasProperty;
+import static org.junit.Assert.assertThat;
 
 public class JavaWpsServiceWriterTest {
 
@@ -31,7 +36,7 @@ public class JavaWpsServiceWriterTest {
     private FileSystem fs;
 
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
         this.wpsServiceWriter = new JavaWpsServiceWriter();
         this.fs = Jimfs.newFileSystem(Configuration.unix());
     }
@@ -54,6 +59,36 @@ public class JavaWpsServiceWriterTest {
         ex.expect(InvocationTargetException.class);
         ex.expectCause(allOf(isA(UnsatisfiedLinkError.class), hasProperty("message", equalTo("no ZOO in java.library.path"))));
         serviceMethod.invoke(instance, null, null, null);
+    }
+
+    @Test
+    public void generateWpsServiceNonJni() throws Exception {
+        Path jniTemplate = Paths.get(getClass().getResource("/templates/java_launcher.java.ftl").toURI());
+        Path jniTemplateBackup = jniTemplate.resolveSibling("java_launcher.java.ftl.bak");
+        Path nojniTemplate = Paths.get(getClass().getResource("/templates/nojni_java_launcher.java.ftl").toURI());
+
+        Files.copy(jniTemplate, jniTemplateBackup, REPLACE_EXISTING);
+        Files.copy(nojniTemplate, jniTemplate, REPLACE_EXISTING);
+
+        try {
+            FtepServiceDescriptor svc = ExampleServiceDescriptor.getExampleSvc();
+
+            Path wpsService = fs.getPath("test.jar");
+            wpsServiceWriter.generateWpsService(svc, wpsService);
+
+            URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{wpsService.toUri().toURL()});
+            Class<?> svcClass = Class.forName(svc.getId(), true, classLoader);
+            Object instance = svcClass.newInstance();
+
+            // This is what zoo_loader.cgi does...
+            Method serviceMethod = svcClass.getDeclaredMethod(svc.getId(), HashMap.class, HashMap.class, HashMap.class);
+
+            Object invocationResult = serviceMethod.invoke(instance, null, null, null);
+            assertThat(invocationResult, is(3));
+        } finally {
+            Files.copy(jniTemplateBackup, jniTemplate, REPLACE_EXISTING);
+            Files.delete(jniTemplateBackup);
+        }
     }
 
 }
---
+++ f-tep-zoowrapper/src/test/resources/templates/nojni_java_launcher.java.ftl
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+
+public class ${id} {
+
+    public static int ${id}(HashMap conf, HashMap inputs, HashMap outputs) {
+        System.out.println("Calling non-JNI service ${id}");
+        return 3;
+    }
+
+}

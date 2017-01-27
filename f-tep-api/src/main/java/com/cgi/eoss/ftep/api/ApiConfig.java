debfd4232a64270b7261e140caabb232bff6ee60
Peter van Zetten <peter.van.zetten@cgi.com>
2017-01-27 11:33:53 +0000

    Add security stub for Java API
    Prepare for securing the Java API with a stub configuration of
    Spring-Security. Currently simply permits all requests.
    
    Change-Id: Idbf143c413421a954a0679c53885a9793ccd3469

--- f-tep-api/src/main/java/com/cgi/eoss/ftep/api/ApiConfig.java
+++ f-tep-api/src/main/java/com/cgi/eoss/ftep/api/ApiConfig.java
@@ -12,6 +12,9 @@ import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import static org.springframework.data.rest.core.mapping.RepositoryDetectionStrategy.RepositoryDetectionStrategies.ANNOTATED;
 
@@ -22,6 +25,7 @@ import static org.springframework.data.rest.core.mapping.RepositoryDetectionStra
         PersistenceConfig.class
 })
 @EnableJpaRepositories(basePackages = "com.cgi.eoss.ftep.api")
+@EnableWebSecurity
 @ComponentScan("com.cgi.eoss.ftep.api")
 public class ApiConfig {
 
@@ -35,4 +39,22 @@ public class ApiConfig {
             }
         };
     }
+
+    @Bean
+    public WebSecurityConfigurerAdapter webSecurityConfigurerAdapter() {
+        return new WebSecurityConfigurerAdapter() {
+            @Override
+            protected void configure(HttpSecurity httpSecurity) throws Exception {
+                // TODO Require shibboleth pre-auth via RequestAttributeAuthenticationFilter
+                // http://docs.spring.io/spring-security/site/docs/4.2.1.RELEASE/reference/htmlsingle/#preauth
+                // TODO Enable per-object ACLs
+                httpSecurity
+                        .authorizeRequests()
+                        .anyRequest().anonymous();
+                httpSecurity
+                        .csrf().disable();
+            }
+        };
+    }
+
 }

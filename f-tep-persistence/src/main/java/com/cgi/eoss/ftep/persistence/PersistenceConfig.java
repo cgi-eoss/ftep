bf5d6a5289032597882c89532b5b8786c295039a
Peter van Zetten <peter.van.zetten@cgi.com>
2017-01-31 16:58:02 +0000

    Use Spring Boot for DataSource configuration
    Change-Id: I93a35a52f9d7f2cea218fc9c409282977b263fc3

--- f-tep-persistence/src/main/java/com/cgi/eoss/ftep/persistence/PersistenceConfig.java
+++ f-tep-persistence/src/main/java/com/cgi/eoss/ftep/persistence/PersistenceConfig.java
@@ -1,27 +1,20 @@
 package com.cgi.eoss.ftep.persistence;
 
-import com.google.common.collect.ImmutableMap;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
-import java.util.Properties;
 
 @Configuration
 @Import({
         PropertyPlaceholderAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
 })
 @EnableJpaRepositories(basePackages = "com.cgi.eoss.ftep.persistence.dao",
         excludeFilters = {@ComponentScan.Filter(SpringJpaRepositoryIgnore.class)})
@@ -29,27 +22,5 @@ import java.util.Properties;
 @EntityScan(basePackages = "com.cgi.eoss.ftep.model")
 @ComponentScan("com.cgi.eoss.ftep.persistence")
 public class PersistenceConfig {
-
-    @Bean
-    @Primary
-    public DataSource dataSource(
-            @Value("${jdbc.url}") String url,
-            @Value("${jdbc.user}") String user,
-            @Value("${jdbc.password}") String password,
-            @Value("${jdbc.dataSourceClassName}") String dataSourceClassName) {
-        Properties dataSourceProperties = new Properties();
-        dataSourceProperties.putAll(ImmutableMap.builder()
-                .put("url", url)
-                .build());
-
-        HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setDataSourceProperties(dataSourceProperties);
-        hikariConfig.setDataSourceClassName(dataSourceClassName);
-        hikariConfig.setUsername(user);
-        hikariConfig.setPassword(password);
-        hikariConfig.setMaximumPoolSize(8);
-
-        return new HikariDataSource(hikariConfig);
-    }
 
 }

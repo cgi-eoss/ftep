4b115a2b132467871f4f805a8b4b88d6d899a15f
Peter van Zetten <peter.van.zetten@cgi.com>
2017-03-01 15:13:52 +0000

    Create additional /api/currentUser methods
    Change-Id: I64ca6f7f1ec131eabdf7a51e88dba3ac9c1c0b6a

--- f-tep-api/src/main/java/com/cgi/eoss/ftep/api/controllers/CurrentUserApi.java
+++ f-tep-api/src/main/java/com/cgi/eoss/ftep/api/controllers/CurrentUserApi.java
@@ -1,10 +1,16 @@
 package com.cgi.eoss.ftep.api.controllers;
 
+import com.cgi.eoss.ftep.model.Group;
 import com.cgi.eoss.ftep.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/currentUser")
@@ -20,6 +26,18 @@ public class CurrentUserApi {
     @GetMapping
     public User currentUser() {
         return ftepSecurityUtil.getCurrentUser();
+    }
+
+    @GetMapping("/grantedAuthorities")
+    public List<String> grantedAuthorities() {
+        return ftepSecurityUtil.getCurrentAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/groups")
+    public Set<Group> groups() {
+        return ftepSecurityUtil.getCurrentGroups();
     }
 
 }
--- f-tep-api/src/main/java/com/cgi/eoss/ftep/api/controllers/FtepSecurityUtil.java
+++ f-tep-api/src/main/java/com/cgi/eoss/ftep/api/controllers/FtepSecurityUtil.java
@@ -1,11 +1,17 @@
 package com.cgi.eoss.ftep.api.controllers;
 
 import com.cgi.eoss.ftep.model.FtepEntityWithOwner;
+import com.cgi.eoss.ftep.model.Group;
 import com.cgi.eoss.ftep.model.User;
 import com.cgi.eoss.ftep.persistence.service.UserDataService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Set;
 
 /**
  * <p>Provides common utility-style methods for interacting with the F-TEP security context.</p>
@@ -25,8 +31,20 @@ public class FtepSecurityUtil {
     }
 
     public User getCurrentUser() {
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        String username = getCurrentAuthentication().getName();
         return userDataService.getByName(username);
+    }
+
+    public Collection<? extends GrantedAuthority> getCurrentAuthorities() {
+        return getCurrentAuthentication().getAuthorities();
+    }
+
+    public Set<Group> getCurrentGroups() {
+        return userDataService.getGroups(getCurrentUser());
+    }
+
+    private Authentication getCurrentAuthentication() {
+        return SecurityContextHolder.getContext().getAuthentication();
     }
 
 }

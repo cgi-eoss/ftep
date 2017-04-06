e2c0a55974bc02370bb41bb30dc2930785982441
Peter van Zetten <peter.van.zetten@cgi.com>
2017-04-06 21:48:50 +0100

    Launch SNAP with inputfiles loaded
    Change-Id: I9702fc8c39a800738d8c1d28b375c175c84f453b

--- f-tep-defaultservices/src/main/resources/SNAP/startapp.sh
+++ f-tep-defaultservices/src/main/resources/SNAP/startapp.sh
@@ -1,1 +1,8 @@
-snap
+
+WORKER_DIR="/home/worker"
+IN_DIR="${WORKER_DIR}/workDir/inDir"
+OUT_DIR="${WORKER_DIR}/workDir/outDir"
+
+INPUT_FILES=$(ls -1 ${IN_DIR}/inputfile/*/*.xml | grep -v 'INSPIRE.xml')
+
+/opt/snap/bin/snap --open ${INPUT_FILES}

a9145fa78732058e163a3c60ffd1ae7824475bb9
Peter van Zetten <peter.van.zetten@cgi.com>
2017-04-10 13:37:12 +0100

    Prevent refData filename conflicts, return correct HTTP responses
    Change-Id: I80a56c72dcd200d6625a49d3e9f7227f7620a8c3

--- f-tep-api/src/main/java/com/cgi/eoss/ftep/api/controllers/FtepFilesApiExtension.java
+++ f-tep-api/src/main/java/com/cgi/eoss/ftep/api/controllers/FtepFilesApiExtension.java
@@ -2,9 +2,11 @@ package com.cgi.eoss.ftep.api.controllers;
 
 import com.cgi.eoss.ftep.api.security.FtepSecurityService;
 import com.cgi.eoss.ftep.catalogue.CatalogueService;
+import com.cgi.eoss.ftep.catalogue.CatalogueUri;
 import com.cgi.eoss.ftep.model.FtepFile;
+import com.cgi.eoss.ftep.model.User;
 import com.cgi.eoss.ftep.model.internal.ReferenceDataMetadata;
-import com.google.common.base.Preconditions;
+import com.cgi.eoss.ftep.persistence.service.FtepFileDataService;
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.io.ByteStreams;
@@ -16,7 +18,9 @@ import org.springframework.data.rest.webmvc.BasePathAwareController;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.hateoas.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -30,6 +34,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URI;
+import java.util.Objects;
 
 /**
  * <p>A {@link RepositoryRestController} for interacting with {@link com.cgi.eoss.ftep.model.FtepFile}s. Extends the
@@ -43,34 +49,47 @@ import java.io.IOException;
 public class FtepFilesApiExtension {
 
     private final FtepSecurityService ftepSecurityService;
+    private final FtepFileDataService ftepFileDataService;
     private final CatalogueService catalogueService;
 
     @PostMapping("/externalProduct")
     @ResponseBody
-    public Resource<FtepFile> saveExternalProductMetadata(@RequestBody GeoJsonObject geoJson) throws Exception {
+    public ResponseEntity saveExternalProductMetadata(@RequestBody GeoJsonObject geoJson) throws Exception {
         FtepFile ftepFile = catalogueService.indexExternalProduct(geoJson);
-        return new Resource<>(ftepFile);
+        return ResponseEntity.created(ftepFile.getUri()).body(new Resource<>(ftepFile));
     }
 
     @PostMapping("/refData")
     @ResponseBody
-    public Resource<FtepFile> saveRefData(
-            @RequestParam("geometry") String geometry,
-            @RequestParam("file") MultipartFile file) throws Exception {
-        try {
-            Preconditions.checkArgument(!Strings.isNullOrEmpty(file.getOriginalFilename()), "Uploaded filename may not be null {}", file);
+    public ResponseEntity saveRefData(@RequestParam("geometry") String geometry, @RequestParam("file") MultipartFile file) throws Exception {
+        User owner = ftepSecurityService.getCurrentUser();
+        String filename = file.getOriginalFilename();
 
+        if (Strings.isNullOrEmpty(filename)) {
+            return ResponseEntity.badRequest().body(String.format("Uploaded filename may not be null %s", file));
+        }
+
+        URI uri = CatalogueUri.REFERENCE_DATA.build(
+                ImmutableMap.of(
+                        "ownerId", owner.getId().toString(),
+                        "filename", filename));
+
+        if (!Objects.isNull(ftepFileDataService.getByUri(uri))) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(String.format("Reference data filename '%s' already exists for user %s", filename, owner.getName()));
+        }
+
+        try {
             ReferenceDataMetadata metadata = ReferenceDataMetadata.builder()
-                    .owner(ftepSecurityService.getCurrentUser())
-                    .filename(file.getOriginalFilename())
+                    .owner(owner)
+                    .filename(filename)
                     .geometry(geometry)
                     .properties(ImmutableMap.of()) // TODO Collect user-driven metadata properties
                     .build();
 
             FtepFile ftepFile = catalogueService.ingestReferenceData(metadata, file);
-            return new Resource<>(ftepFile);
+            return ResponseEntity.created(ftepFile.getUri()).body(new Resource<>(ftepFile));
         } catch (Exception e) {
-            LOG.error("Could not ingest reference data file {}", file.getOriginalFilename(), e);
+            LOG.error("Could not ingest reference data file {}", filename, e);
             throw e;
         }
     }

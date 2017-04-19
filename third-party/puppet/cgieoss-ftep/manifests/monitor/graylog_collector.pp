208ed1780645a7222718a943b01043349422d0b2
Peter van Zetten <peter.van.zetten@cgi.com>
2017-04-19 16:40:32 +0100

    Fix graylog_collector puppet module and RPM
    Upgrade graylog collector to 0.5.0 and fix configuration.
    
    Change-Id: I6cdabe93cad19bac8cf7d367e2e53b2e7099abb9

--- third-party/puppet/cgieoss-ftep/manifests/monitor/graylog_collector.pp
+++ third-party/puppet/cgieoss-ftep/manifests/monitor/graylog_collector.pp
@@ -1,31 +1,43 @@
-class ftep::monitor::graylog_collector(
-  $log_path = '/var/log',
-  $log_pattern = '*.log',
-  $graylog_server = undef
+class ftep::monitor::graylog_collector (
+  $enable_syslog_collector = true,
+  $enable_apache_collector = false,
+  $graylog_api_url         = undef,
+  $graylog_server          = undef,
+  $graylog_gelf_tcp_port   = undef,
 ) {
 
   require ::ftep::globals
 
+  $config_file = '/etc/graylog/collector/collector.conf'
+
+  $real_graylog_api_url = pick($graylog_api_url, "${ftep::globals::base_url}${ftep::globals::graylog_api_path}")
   $real_graylog_server = pick($graylog_server, $ftep::globals::monitor_hostname)
+  $real_graylog_gelf_tcp_port = pick($graylog_gelf_tcp_port, $ftep::globals::graylog_gelf_tcp_port)
 
-  package { 'graylog-collector':
-      ensure => 'installed'
-  } ->
+  ensure_packages(['graylog-collector'], {
+    ensure => 'present',
+  })
 
-    file { '/etc/graylog/collector/collector.conf':
-        ensure => present,
-        mode => 640,
-        content => epp('ftep/graylog-collector/collector.conf.epp', {
-            'log_path'               => $log_path,
-            'log_pattern'            => $log_pattern,
-            'graylog_server'         => $real_graylog_server,
-            '$graylog_port'          => $graylog_port,
-            '$graylog_api_path'      => $graylog_api_path,
-            '$graylog_gelf_tcp_port' => $graylog_gelf_tcp_port,
-            '$graylog_context_path'  => $graylog_context_path
-        })
-    } -> 
-      service { 'graylog-collector':
-          ensure => 'running'
-      }
+  file { $config_file:
+    ensure  => present,
+    mode    => '640',
+    content => epp('ftep/graylog-collector/collector.conf.epp', {
+      'enable_syslog_collector' => $enable_syslog_collector,
+      'enable_apache_collector' => $enable_apache_collector,
+      'graylog_api_url'         => $real_graylog_api_url,
+      'graylog_server'          => $real_graylog_server,
+      'graylog_gelf_tcp_port'   => $real_graylog_gelf_tcp_port,
+    }),
+    require => Package['graylog-collector'],
+    notify  => Service['graylog-collector'],
+  }
+
+  service { 'graylog-collector':
+    ensure => 'running',
+    enable => true,
+    hasrestart => true,
+    hasstatus  => true,
+    require => [Package['graylog-collector'], File[$config_file]],
+  }
+
 }

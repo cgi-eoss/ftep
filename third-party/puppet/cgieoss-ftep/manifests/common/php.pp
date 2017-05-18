2cb5349c653651d577efe7d70e7e99b682f84532
Peter van Zetten <peter.van.zetten@cgi.com>
2017-05-18 14:32:51 +0100

    Increase PHP memory limit
    Change-Id: I2621e3845ad0ace3cae9982a0ed9e5a15d5130c7

--- third-party/puppet/cgieoss-ftep/manifests/common/php.pp
+++ third-party/puppet/cgieoss-ftep/manifests/common/php.pp
@@ -28,7 +28,7 @@ class ftep::common::php {
     settings       => {
       'PHP/max_execution_time'  => '90',
       'PHP/max_input_time'      => '300',
-      'PHP/memory_limit'        => '64M',
+      'PHP/memory_limit'        => '128M',
       'PHP/post_max_size'       => '32M',
       'PHP/upload_max_filesize' => '32M',
       'Date/date.timezone'      => 'UTC',

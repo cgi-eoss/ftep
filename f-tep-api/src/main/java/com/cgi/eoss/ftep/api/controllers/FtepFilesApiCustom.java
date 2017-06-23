fa38e134b27beb6836b2c5e35ad9f27050e4598a
Peter van Zetten <peter.van.zetten@cgi.com>
2017-06-23 14:15:31 +0100

    Fully delete FtepFiles from catalogues
    Change-Id: I77c69e2520485cd88ea0078a983900c9ef8a2c2b

--- f-tep-api/src/main/java/com/cgi/eoss/ftep/api/controllers/FtepFilesApiCustom.java
+++ f-tep-api/src/main/java/com/cgi/eoss/ftep/api/controllers/FtepFilesApiCustom.java
@@ -6,6 +6,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface FtepFilesApiCustom {
+    void delete(FtepFile ftepFile);
+
     Page<FtepFile> findByType(FtepFile.Type type, Pageable pageable);
 
     Page<FtepFile> findByFilterOnly(String filter, FtepFile.Type type, Pageable pageable);

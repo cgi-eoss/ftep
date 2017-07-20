07d670c5023576a2b62d4002683314f589ecfd96
Peter van Zetten <peter.van.zetten@cgi.com>
2017-07-20 10:39:36 +0100

    Replace embedded cgieoss-ftep puppet module with git submodule
    Change-Id: I13a6203dc9875576005256ad3a9544bcccb19b18

--- third-party/puppet/cgieoss-ftep/manifests/db.pp
+++
@@ -1,9 +0,0 @@
-class ftep::db(
-  $trust_local_network = false,
-) {
-
-  require ::ftep::globals
-
-  contain ::ftep::db::postgresql
-
-}
--- third-party/puppet/cgieoss-ftep/spec/classes/ftep__portal_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-#Puppet::Util::Log.level = :debug
-#Puppet::Util::Log.newdestination(:console)
-
-describe 'ftep::portal', :type => 'class' do
-  it { should compile }
-  it { should contain_class('ftep::portal') }
-end

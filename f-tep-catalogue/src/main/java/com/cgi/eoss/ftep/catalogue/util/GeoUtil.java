05ddadc03cd37876166794edd973c0d97ed76665
Peter van Zetten <peter.van.zetten@cgi.com>
2017-08-15 10:07:55 +0100

    Add bounding box extraction for shapefiles and geotiffs
    Change-Id: I17290dca6cc6fd9767b85496b4f5c4544f11f6d3

--- f-tep-catalogue/src/main/java/com/cgi/eoss/ftep/catalogue/util/GeoUtil.java
+++ f-tep-catalogue/src/main/java/com/cgi/eoss/ftep/catalogue/util/GeoUtil.java
@@ -2,14 +2,27 @@ package com.cgi.eoss.ftep.catalogue.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
+import com.vividsolutions.jts.geom.Polygon;
 import lombok.experimental.UtilityClass;
 import lombok.extern.log4j.Log4j2;
 import org.geojson.GeoJsonObject;
 import org.geojson.LngLatAlt;
+import org.geotools.coverage.grid.io.AbstractGridCoverage2DReader;
+import org.geotools.coverage.grid.io.AbstractGridFormat;
+import org.geotools.coverage.grid.io.GridFormatFinder;
+import org.geotools.coverage.grid.io.UnknownFormat;
+import org.geotools.data.DataStore;
+import org.geotools.data.DataStoreFinder;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.factory.Hints;
 import org.geotools.geometry.GeometryBuilder;
+import org.geotools.geometry.jts.JTS;
+import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.geotools.geometry.text.WKTParser;
 import org.geotools.referencing.crs.DefaultGeographicCRS;
+import org.opengis.filter.Filter;
 import org.opengis.geometry.coordinate.LineString;
 import org.opengis.geometry.coordinate.PointArray;
 import org.opengis.geometry.coordinate.Position;
@@ -17,6 +30,8 @@ import org.opengis.geometry.primitive.Curve;
 import org.opengis.geometry.primitive.Point;
 import org.opengis.geometry.primitive.Surface;
 
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,7 +43,9 @@ import java.util.stream.Collectors;
 public class GeoUtil {
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-    private static final WKTParser WKT_PARSER = new WKTParser(new GeometryBuilder(DefaultGeographicCRS.WGS84));
+    private static final GeometryBuilder GEOMETRY_BUILDER = new GeometryBuilder(DefaultGeographicCRS.WGS84);
+
+    private static final WKTParser WKT_PARSER = new WKTParser(GEOMETRY_BUILDER);
 
     private static final String DEFAULT_POINT = "POINT(0 0)";
 
@@ -92,5 +109,37 @@ public class GeoUtil {
             LOG.error("Could not deserialise GeoJsonObject: {}", geojson, e);
             throw new RuntimeException(e);
         }
+    }
+
+    public static org.geojson.Polygon extractBoundingBox(Path file) {
+        try {
+            ReferencedEnvelope envelope = getEnvelope(file);
+
+            Polygon polygon = JTS.toGeometry(envelope.toBounds(DefaultGeographicCRS.WGS84));
+            LOG.debug("Extracted WKT bounding box from file {}: {}", file.getFileName(), polygon);
+            return wktToGeojsonPolygon(polygon.toString());
+        } catch (Exception e) {
+            LOG.error("Could not extract bounding box from file: {}", file, e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static ReferencedEnvelope getEnvelope(Path file) throws IOException {
+        // General metadata
+        DataStore dataStore = DataStoreFinder.getDataStore(ImmutableMap.of("url", file.toUri().toURL()));
+        if (dataStore != null) {
+            SimpleFeatureCollection featureCollection = dataStore.getFeatureSource(dataStore.getTypeNames()[0]).getFeatures(Filter.INCLUDE);
+            return featureCollection.getBounds();
+        }
+
+        // Raster data
+        AbstractGridFormat gridFormat = GridFormatFinder.findFormat(file.toUri().toURL());
+        if (gridFormat != null && !(gridFormat instanceof UnknownFormat)) {
+            Hints hints = new Hints(Hints.FORCE_LONGITUDE_FIRST_AXIS_ORDER, Boolean.TRUE);
+            AbstractGridCoverage2DReader reader = gridFormat.getReader(file.toUri().toURL(), hints);
+            return ReferencedEnvelope.reference(reader.getOriginalEnvelope());
+        }
+
+        throw new UnsupportedOperationException("Could not extract geometry envelope from " + file.toString());
     }
 }
--- f-tep-catalogue/src/test/java/com/cgi/eoss/ftep/catalogue/util/GeoUtilTest.java
+++ f-tep-catalogue/src/test/java/com/cgi/eoss/ftep/catalogue/util/GeoUtilTest.java
@@ -5,6 +5,8 @@ import org.geojson.Point;
 import org.geojson.Polygon;
 import org.junit.Test;
 
+import java.nio.file.Paths;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -25,6 +27,7 @@ public class GeoUtilTest {
 
         assertThat(polygon, is(expected));
     }
+
     @Test
     public void testWktToGeojsonPoint() throws Exception {
         Point point = GeoUtil.wktToGeojsonPoint("POINT(0 0)");
@@ -32,6 +35,48 @@ public class GeoUtilTest {
 
         assertThat(point, is(expected));
         assertThat(GeoUtil.defaultPoint(), is(expected));
+    }
+
+    @Test
+    public void extractBoundingBoxShapefileChiapas() throws Exception {
+        Polygon polygon = GeoUtil.extractBoundingBox(Paths.get(getClass().getResource("/F-TEPchiapasLandCover/F-TEPchiapasLandCover.shp").toURI()));
+        Polygon expected = new Polygon(
+                new LngLatAlt(-92.91927146994584, 15.779673670302039),
+                new LngLatAlt(-92.30984753765863, 15.779673670302039),
+                new LngLatAlt(-92.30984753765863, 16.255487070951077),
+                new LngLatAlt(-92.91927146994584, 16.255487070951077),
+                new LngLatAlt(-92.91927146994584, 15.779673670302039)
+        );
+
+        assertThat(polygon, is(expected));
+    }
+
+    @Test
+    public void extractBoundingBoxShapefileDurango() throws Exception {
+        Polygon polygon = GeoUtil.extractBoundingBox(Paths.get(getClass().getResource("/DurangoRef/F-TEPsampleDurangoLandCoverRefPolygons.shp").toURI()));
+        Polygon expected = new Polygon(
+                new LngLatAlt(-104.88452021695508, 23.547911691931287),
+                new LngLatAlt(-104.03747413959455, 23.547911691931287),
+                new LngLatAlt(-104.03747413959455, 24.390684514620556),
+                new LngLatAlt(-104.88452021695508, 24.390684514620556),
+                new LngLatAlt(-104.88452021695508, 23.547911691931287)
+        );
+
+        assertThat(polygon, is(expected));
+    }
+
+    @Test
+    public void extractBoundingBoxGeotiff() throws Exception {
+        Polygon polygon = GeoUtil.extractBoundingBox(Paths.get(getClass().getResource("/subset_0_of_S2A_OPER_MTD_SAFL1C_PDMC_20160521T232007_R055_V20160521T174723_20160521T174723_resampled_RGB.tif").toURI()));
+        Polygon expected = new Polygon(
+                new LngLatAlt(-106.9893803701548, 23.945607806036303),
+                new LngLatAlt(-106.97919638534522, 23.945607806036303),
+                new LngLatAlt(-106.97919638534522, 23.954967318294422),
+                new LngLatAlt(-106.9893803701548, 23.954967318294422),
+                new LngLatAlt(-106.9893803701548, 23.945607806036303)
+        );
+
+        assertThat(polygon, is(expected));
     }
 
 }

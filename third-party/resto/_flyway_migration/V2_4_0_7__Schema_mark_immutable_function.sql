01b908a52b40dd309309198ce5f94538b20c7a0c
Peter van Zetten <peter.vanzetten@gmail.com>
2018-02-09 11:26:06 +0000

    Fix immutable index functions in resto db schema
    Change-Id: Id573200cbff933b1c3ad70789b6fde2ba073ed60

---
+++ third-party/resto/_flyway_migration/V2_4_0_7__Schema_mark_immutable_function.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE FUNCTION f_unaccent(text)
+  RETURNS text AS
+$func$
+SELECT public.unaccent('public.unaccent', $1)  -- schema-qualify function and dictionary
+$func$  LANGUAGE sql IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION normalize(text)
+  RETURNS text AS $$
+SELECT replace(replace(lower(public.f_unaccent($1)),' ','-'), '''', '-')
+$$ LANGUAGE sql;

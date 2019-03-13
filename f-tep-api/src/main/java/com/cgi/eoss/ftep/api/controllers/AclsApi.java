cd6f895b4263b1b047e85e5a97660bc944056fcc
Peter van Zetten <peter.van.zetten@cgi.com>
2019-03-13 10:09:11 +0000

    Fix Databasket sharing permission check
    Change-Id: I3151964015faa8a29f4d89b89589bffa0e1336a1

--- f-tep-api/src/main/java/com/cgi/eoss/ftep/api/controllers/AclsApi.java
+++ f-tep-api/src/main/java/com/cgi/eoss/ftep/api/controllers/AclsApi.java
@@ -76,7 +76,7 @@ public class AclsApi {
     }
 
     @GetMapping("/databasket/{databasketId}")
-    @PreAuthorize("hasAnyRole('CONTENT_AUTHORITY', 'ADMIN') or hasPermission(#databasketId, T(com.cgi.eoss.ftep.model.Databasket), 'administration')")
+    @PreAuthorize("hasAnyRole('CONTENT_AUTHORITY', 'ADMIN') or hasPermission(#databasketId, T(com.cgi.eoss.ftep.model.Databasket).name, 'administration')")
     public FtepAccessControlList getDatabasketAcls(@PathVariable("databasketId") Long databasketId) {
         return FtepAccessControlList.builder()
                 .entityId(databasketId)

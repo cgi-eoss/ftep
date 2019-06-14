22932308ab3beb17f776a0c66de6adce2342c36c
Vill <hiie.vill@cgi.com>
2019-06-14 11:44:37 +0300

    Undefined AOI parameter fix
    Change-Id: If9a9380427ea9b15c498570758388b6208c23e88

--- f-tep-portal/src/main/resources/app/scripts/controllers/explorer/sidebar/search.js
+++ f-tep-portal/src/main/resources/app/scripts/controllers/explorer/sidebar/search.js
@@ -39,7 +39,9 @@ define(['../../../ftepmodules'], function(ftepmodules) {
         });
 
         $scope.selectCatalog = function(field, catalog) {
-            $scope.searchParams.activeSearch = {};
+            if (!$scope.searchParams.activeSearch) {
+                $scope.searchParams.activeSearch = {};
+            }
             $scope.searchParams.activeSearch[field.type] = catalog.value;
             $scope.searchParams.selectedCatalog = catalog;
             $scope.updateForm($scope.searchParams.catalogues, $scope.searchParams.activeSearch);
@@ -52,6 +54,9 @@ define(['../../../ftepmodules'], function(ftepmodules) {
 
         $scope.$watch('mapAoi.wkt', function(wkt) {
             if (wkt) {
+                if (!$scope.searchParams.activeSearch) {
+                    $scope.searchParams.activeSearch = {};
+                }
                 $scope.searchParams.activeSearch.aoi = wkt;
             }
         });

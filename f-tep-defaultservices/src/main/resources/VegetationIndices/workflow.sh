62d9b60f50c424f8fde2e8266359b7d3b13d54a3
Peter van Zetten <peter.vanzetten@gmail.com>
2021-02-23 10:15:23 +0000

    Update VegetationIndices default service
    Change-Id: Id8753989d980dabc56c04ff21f24c73330959920

--- f-tep-defaultservices/src/main/resources/VegetationIndices/workflow.sh
+++ f-tep-defaultservices/src/main/resources/VegetationIndices/workflow.sh
@@ -16,60 +16,75 @@ S2PRODUCTZONES="python ${WORKFLOW}/s2ProductZones.py"
 
 mkdir -p ${PROC_DIR}
 mkdir -p ${OUT_DIR}/result
+#mkdir -p "${OUT_DIR}/result_${VEG_INDEX}"
 mkdir -p "${PROC_DIR}/preprocessed"
 
 # Input params
 source ${WPS_PROPS}
 AOI="${aoi}"
+INPUTFILE="${inputfile}"
 TARGET_RESOLUTION="${targetResolution:-10}"
 VEG_INDEX="${vegIndex}"
 
+# extract the input product identifier (via bash parameter expansion):
+INPUT_ID=${INPUTFILE##*/}
+
 # Calculated input params
 SCALING_FACTOR=$(echo "scale=2;${TARGET_RESOLUTION}/10" | bc)
-GRANULE_METADATA=$(ls -1 ${IN_DIR}/input/*/GRANULE/*/MTD_TL.xml | head -1)
-EPSG=$(${S2PRODUCTZONES} ${GRANULE_METADATA})
 
 # Internal params
 S2_PREPROCESS="${WORKFLOW}/F-TEP_S2_preprocessNew.xml"
 PREPROCESSED_PREFIX="${PROC_DIR}/preprocessed"
 MOSAIC_OUTPUT="${PREPROCESSED_PREFIX}/mosaic.tif"
 VI_INPUT="${PREPROCESSED_PREFIX}/vi_input.tif"
+# output file name (since 25 Mar 2019; old version saved as a comment):
 VI_OUTPUT="${OUT_DIR}/result/FTEP_VEGETATION_INDICES_${VEG_INDEX}_${TIMESTAMP}.tif"
+#VI_OUTPUT="${OUT_DIR}/result_${VEG_INDEX}/F-TEP_${VEG_INDEX}_${TIMESTAMP}_from_${INPUT_ID}.tif"
 
 # Bounds of given AOI
-if [ "" != "${AOI}" ]; then
-    AOI_EXTENTS=($(${POLYGON2NSEW} "${AOI}"))
-    NORTH_BOUND=${AOI_EXTENTS[0]}
-    SOUTH_BOUND=${AOI_EXTENTS[1]}
-    EAST_BOUND=${AOI_EXTENTS[2]}
-    WEST_BOUND=${AOI_EXTENTS[3]}
-fi
-UL=($(echo "${WEST_BOUND} ${NORTH_BOUND}" | cs2cs +init=epsg:4326 +to +init=epsg:${EPSG#EPSG:}))
-LR=($(echo "${EAST_BOUND} ${SOUTH_BOUND}" | cs2cs +init=epsg:4326 +to +init=epsg:${EPSG#EPSG:}))
-EMIN=${UL[0]}
-NMAX=${UL[1]}
-EMAX=${LR[0]}
-NMIN=${LR[1]}
+gdal_aoi_arg() {
+    if [ "" != "${AOI}" ]; then
+        AOI_EXTENTS=($(${POLYGON2NSEW} "${AOI}"))
+        NORTH_BOUND=${AOI_EXTENTS[0]}
+        SOUTH_BOUND=${AOI_EXTENTS[1]}
+        EAST_BOUND=${AOI_EXTENTS[2]}
+        WEST_BOUND=${AOI_EXTENTS[3]}
+        UL=($(echo "${WEST_BOUND} ${NORTH_BOUND}" | cs2cs +init=epsg:4326 +to +init=epsg:${1#EPSG:}))
+        LR=($(echo "${EAST_BOUND} ${SOUTH_BOUND}" | cs2cs +init=epsg:4326 +to +init=epsg:${1#EPSG:}))
+        EMIN=${UL[0]}
+        NMAX=${UL[1]}
+        EMAX=${LR[0]}
+        NMIN=${LR[1]}
+        GDAL_AOI_ARG="-te ${EMIN} ${NMIN} ${EMAX} ${NMAX}"
+    else
+        GDAL_AOI_ARG=""
+    fi
+}
 
 # Preprocess S2 input: extract correct bands and resample
 I=0
-for IN in ${IN_DIR}/input/*; do
+for IN in ${IN_DIR}/inputfile/*; do
     I=$((I+1))
+    GRANULE_METADATA=$(ls -1 ${IN}/GRANULE/*/MTD_TL.xml | head -1)
+    EPSG=$(${S2PRODUCTZONES} ${GRANULE_METADATA})
     INPUT_FILE=$(ls -1 ${IN}/*.xml | grep -v 'INSPIRE.xml' | head -1)
     time gpt ${S2_PREPROCESS} -Pifile=${INPUT_FILE} -Paoi="${AOI}" -PtargetResolution="${TARGET_RESOLUTION}" -Pofile="${PREPROCESSED_PREFIX}-${I}.tif"
-    time gdalwarp -t_srs ${EPSG} -te $EMIN $NMIN $EMAX $NMAX -tr $TARGET_RESOLUTION $TARGET_RESOLUTION -tap -ot Int16 -dstnodata "0 0 0 0" -r near -of GTiff -overwrite ${PREPROCESSED_PREFIX}-${I}.tif ${PREPROCESSED_PREFIX}-${I}r.tif
+    time gdalwarp -t_srs ${EPSG} $(gdal_aoi_arg ${EPSG}) -tr $TARGET_RESOLUTION $TARGET_RESOLUTION -tap -ot Int16 -dstnodata "0 0 0 0" -r near -of GTiff -overwrite ${PREPROCESSED_PREFIX}-${I}.tif ${PREPROCESSED_PREFIX}-${I}r.tif
 done
+
 if [ 1 == ${I} ]; then
     mv ${PREPROCESSED_PREFIX}-${I}r.tif ${VI_INPUT}
 
-# Execute otb to generate radiometric index
+    # Execute otb to generate radiometric index
     time otbcli_RadiometricIndices -in ${VI_INPUT} -channels.blue 1 -channels.green 2 -channels.red 3 -channels.nir 4 -list "Vegetation:${VEG_INDEX}" -out ${VI_OUTPUT}
     
     #TEST: assign nodata value of -999 instead of 0, so that Geoserver WMS can show transparency with ColorMap styling
     #gdal_edit -a_nodata -999 ${VI_OUTPUT}
     #NOTE:gdal_edit not available in gdal-bin - TODO: try translate
-    
 else
-    echo "Single tile products only, now: ", $I
+	#here, could perform e.g. mosaicking
+    #echo "Single tile products only, now: ", $I
+    echo "NOTE: To process multiple inputs, enable Parallel processing in service execution!"
 fi
+
 exit 0

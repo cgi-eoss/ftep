7f8debc2dc20e2088fdfe0b7d30dba77904b3c6a
Peter van Zetten <peter.van.zetten@cgi.com>
2021-06-23 10:20:13 +0100

    Update ftep-worker-base to Ubuntu 20.04
    Change-Id: I06591f124893363392bb960ab57fb624a50dbc92

--- f-tep-clouds/src/main/resources/worker_image/install.sh
+++ f-tep-clouds/src/main/resources/worker_image/install.sh
@@ -2,9 +2,12 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-sudo apt-get -y update
-sudo apt-get -y dist-upgrade
-sudo apt-get -y install --no-install-recommends apt-transport-https ca-certificates curl gnupg-agent software-properties-common nfs-common
+while ! sudo apt -y update; do
+  sleep 1
+done
+
+sudo apt -y dist-upgrade
+sudo apt -y install --no-install-recommends apt-transport-https ca-certificates curl gpg-agent software-properties-common nfs-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
@@ -12,5 +15,5 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 
-sudo apt-get -y update
-sudo apt-get -y install --no-install-recommends docker-ce docker-ce-cli containerd.io
+sudo apt -y update
+sudo apt -y install --no-install-recommends docker-ce docker-ce-cli containerd.io
